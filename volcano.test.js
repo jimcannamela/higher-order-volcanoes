af9fe2356629d1245672b0a86480da4e0e6c0082
Tanner Hess Webber <tb@hesswebber.com>
2019-04-17 20:26:11 +0530

    fix(test): clear mocks before any test and refactor testing utility method for correct behavior

--- volcano.test.js
+++ volcano.test.js
@@ -5,13 +5,16 @@ const log = jest.spyOn(console, 'log').mockImplementation(() => {});
 
 // ? allows instructors to return or console.log while live-coding
 const captureResult = (func, ...args) => {
-  const result = func(...args); 
-  return log.mock.calls[0] ? log.mock.calls[0][0] : result
+  const result = func(...args);
+  return log.mock.calls[0] ? log.mock.calls[0][0] : result;
 };
 
 const VA = new VolcanoAnalyzer();
 
 describe('VolcanoAnalyzer', () => {
+  beforeEach(() => {
+    log.mockClear();
+  });
   it('should list volcanoes that erupted in the 80s', () => {
     expect(captureResult(VA.eruptedInEighties)).toMatchSnapshot();
   });
