749a9bff84ab5fc7c738d8c1adadbdddeb4db4f7
culleton <ian.culleton.92@gmail.com>
2023-03-06 10:21:34 -0500

    removes VolcanoAnalyzer class

--- volcano.js
+++ volcano.js
@@ -1,77 +1,74 @@
 const volcanoes = require('./volcano.json');
 
-class VolcanoAnalyzer {
-  eruptedInEighties() {
-    // TODO: return the volcanoes that erupted in the 1980s.
-    let result;
-    console.log(result);
-  }
-
-  highVEI() {
-    // TODO: return an array of the names of volcanoes that have a VEI of 6 or higher.
-    let result;
-    console.log(result);
-  }
-
-  mostDeadly() {
-    // TODO: return the volcano with the highest recorded deaths.
-    let result;
-    console.log(result);
-  }
-
-  causedTsunami() {
-    // TODO: return the percentage of volcanoes that caused tsunamis.
-    let result;
-    console.log(result);
-  }
-
-  mostCommonType() {
-    // TODO: return the most common type of volcano in the list
-    let result;
-    console.log(result);
-  }
-
-  eruptionsByCountry(country) {
-    // TODO: return the number of eruptions when supplied a country as an argument
-    let result;
-    console.log(result);
-  }
-
-  averageElevation() {
-    // TODO: return the average elevation of all volcanoes
-    let result;
-    console.log(result);
-  }
-
-  volcanoTypes() {
-    // TODO: return an array of types of volcanoes
-    let result;
-    console.log(result);
-  }
-
-  percentNorth() {
-    // TODO: return the percentage of eruptions that occurred in the Northern hemisphere
-    let result;
-    console.log(result);
-  }
-
-  manyFilters() {
-    // TODO: return names of eruptions that occurred after 1800, that did not cause a tsunami, happened in the Southern hemisphere, and had a VEI of 5.
-    let result;
-    console.log(result);
-  }
-
-  elevatedVolcanoes(elevation) {
-    // TODO: return names of eruptions that occurred at or above an elevation passed as an argument
-    let result;
-    console.log(result);
-  }
-
-  topAgentsOfDeath(elevation) {
-    // TODO: return the unique agents of death in the top ten deadliest eruptions
-    let result;
-    console.log(result);
-  }
+const eruptedInEighties = () => {
+  // TODO: return the volcanoes that erupted in the 1980s.
+  let result;
+  console.log(result);
 }
 
-module.exports = VolcanoAnalyzer;
+const highVEI = () => {
+  // TODO: return an array of the names of volcanoes that have a VEI of 6 or higher.
+  let result;
+  console.log(result);
+}
+
+const mostDeadly = () => {
+  // TODO: return the volcano with the highest recorded deaths.
+  let result;
+  console.log(result);
+}
+
+const causedTsunami = () => {
+  // TODO: return the percentage of volcanoes that caused tsunamis.
+  let result;
+  console.log(result);
+}
+
+const mostCommonType = () => {
+  // TODO: return the most common type of volcano in the list
+  let result;
+  console.log(result);
+}
+
+const eruptionsByCountry = (country) => {
+  // TODO: return the number of eruptions when supplied a country as an argument
+  let result;
+  console.log(result);
+}
+
+const averageElevation = () => {
+  // TODO: return the average elevation of all volcanoes
+  let result;
+  console.log(result);
+}
+
+const volcanoTypes = () => {
+  // TODO: return an array of types of volcanoes
+  let result;
+  console.log(result);
+}
+
+const percentNorth = () => {
+  // TODO: return the percentage of eruptions that occurred in the Northern hemisphere
+  let result;
+  console.log(result);
+}
+
+const manyFilters = () => {
+  // TODO: return names of eruptions that occurred after 1800, that did not cause a tsunami, happened in the Southern hemisphere, and had a VEI of 5.
+  let result;
+  console.log(result);
+}
+
+const elevatedVolcanoes = (elevation) => {
+  // TODO: return names of eruptions that occurred at or above an elevation passed as an argument
+  let result;
+  console.log(result);
+}
+
+const topAgentsOfDeath = (elevation) => {
+  // TODO: return the unique agents of death in the top ten deadliest eruptions
+  let result;
+  console.log(result);
+}
+
